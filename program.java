bc738d175a817a8d2030aa2861679e2f73ed6375
srinidhi123c <srinidhis23576@gmail.com>
2024-05-06 16:08:38 +0530

    find the output

---
+++ program.java
@@ -0,0 +1,16 @@
+public class program {
+
+        public static void main(String[] args) {
+            
+            int count=0;
+            for(int i=1;i<=10;i++)
+            {
+                count=count++;
+            }
+            System.out.println("print the count :"+count);
+    
+        }
+    
+    }
+    
+
